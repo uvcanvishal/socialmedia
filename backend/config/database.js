b4519877048f7e68309bc8f00e3e4f2607edf883
Vishal kumar singh <uvcanvishal@gmail.com>
2022-04-27 21:42:13 +0530

    just changed the database file

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 exports.connectDatabase = ()=>{
-    mongoose.connect(process.env.MONGO_URI)
-    .then(con=>console.log(`Database Connected: ${con.connection.host}`))
+    mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true,
+    }).then(con=>console.log(`Database Connected: ${con.connection.host}`))
     .catch((err)=>console.log(err));
 }
